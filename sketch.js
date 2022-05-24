6fc43f97b99133266473a37b71e564cf8f34678f
NamanP18 <99287749+NamanP18@users.noreply.github.com>
2022-05-24 19:46:00 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,11 +17,9 @@ function setup() {
 
   playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
 //create a player object from the Player class.
-  player= new Player(
-    width - 300,
-    random(450, height - 300),
-    180,
-    150)
+  player= new Player( 280, random(450, height - 300), 180, 150)
+    
+    
 
   computerBase = new ComputerBase(
     width - 300,
